bdbb6f815a83626dfbd78a9c801b69eabd6940c7
sarfarajansari <sarfarajansaridev@gmail.com>
2024-08-15 09:59:45 +0530

    added mp3 downloader

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
+import yt_dlp
+import os
 
-from youtube_transcript_api import YouTubeTranscriptApi
+import os
+import urllib.request
+import zipfile
+
+import yt_dlp
+
+def download_audio(video_url, output_path='downloads'):
+    # yt-dlp options
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',  
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
 
 
-transcript = YouTubeTranscriptApi.get_transcript("P9t1qBSf4-U")
+url="https://www.youtube.com/watch?v=RQ2nYUBVvqI"
+if __name__ == '__main__':
+    video_url = input("Enter the YouTube video URL: ")
+    download_audio(video_url or url)
 
-print(transcript)
+    
